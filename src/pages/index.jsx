20516beadb45edb3d461648221b4d3f757298fa1
DanielGustavo <dg520589@gmail.com>
2021-10-23 14:17:36 -0300

    feat(homepage): send user to '/users/[username]' when form is submited

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,4 +1,5 @@
-import Image from 'next/image'
+import Router from 'next/router';
+import Image from 'next/image';
 
 import Logo from '../assets/images/Logo.png';
 
@@ -6,20 +7,31 @@ import Input from '../components/Input';
 
 import styles from '../styles/home.module.css';
 
-const HomePage = () => (
-  <div className={`${styles.container} container`}>
-    <Image src={Logo} alt="Github Searcher" width={303} height={148} />
+const HomePage = () => {
+  function handleSubmit(event) {
+    event.preventDefault();
 
-    <div className={styles.inputContainer}>
-      <Input
-        className={styles.input}
-        buttonIcon="search"
-        name="username"
-        placeholder="Insert a username"
-      />
-      <button className={styles.button}>Search</button>
+    const form = event.target;
+    const { username: { value: username } } = form;
+
+    Router.push(`/users/${username}`);
+  }
+
+  return (
+    <div className={`${styles.container} container`}>
+      <Image src={Logo} alt="Github Searcher" width={303} height={148} />
+
+      <form onSubmit={handleSubmit} className={styles.inputContainer}>
+        <Input
+          className={styles.input}
+          buttonIcon="search"
+          name="username"
+          placeholder="Insert a username"
+        />
+        <button className={styles.button}>Search</button>
+      </form>
     </div>
-  </div>
-)
+  );
+};
 
-export default HomePage
+export default HomePage;

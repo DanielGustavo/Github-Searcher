a5578cd6057e4eb7b51604f2880d2a8d9e756b11
DanielGustavo <dg520589@gmail.com>
2021-10-26 06:53:43 -0300

    feat: add some methods in githubService
    add 'getRepositoriesFromUsername' and 'getStarsFromUsername'

--- src/services/githubService.js
+++ src/services/githubService.js
@@ -5,3 +5,22 @@ export async function getUserFromUsername(username) {
 
   return user;
 }
+
+export async function getRepositoriesFromUsername(username) {
+  const repositories = await api.get(`https://api.github.com/users/${username}/repos`)
+
+  return repositories;
+}
+
+export async function getStarsFromUsername(username) {
+  let stars = 0;
+
+  await fetch(`https://api.github.com/users/${username}/starred?per_page=1`).then(res => {
+    stars = res.headers.get('link').split(',')[1].split('&page=')[1].split('>')[0];
+    stars = parseInt(stars)
+  });
+
+  return stars;
+}
+
+

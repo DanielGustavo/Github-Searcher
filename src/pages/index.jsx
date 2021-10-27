8b6a1eeff412f7148201b8745a0a78859e91a965
DanielGustavo <dg520589@gmail.com>
2021-10-27 20:11:12 -0300

    feat: add loading animation in homepage when searching a user

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,21 +1,31 @@
+import { useState } from 'react';
 import Router from 'next/router';
+import ReactLoading from 'react-loading';
 
 import Input from '../components/Input';
 
 import styles from '../styles/home.module.css';
 
 const HomePage = () => {
+  const [loading, setLoading] = useState(false);
+
   function handleSubmit(event) {
     event.preventDefault();
 
+    setLoading(true);
+
     const form = event.target;
-    const {
-      username: { value: username },
-    } = form;
+    const { username: usernameInput } = form;
+
+    const username = usernameInput.value;
 
     if (username) {
       Router.push(`/users/${username}`);
+    } else {
+      setLoading(false);
     }
+
+    usernameInput.blur();
   }
 
   return (
@@ -29,6 +39,8 @@ const HomePage = () => {
         />
         <button className={styles.button}>Search</button>
       </form>
+
+      {loading && <ReactLoading type="spin" color="var(--primary-color)" />}
     </div>
   );
 };

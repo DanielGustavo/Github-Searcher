537eb1eab90a56d2273923797af10fad680ca751
DanielGustavo <dg520589@gmail.com>
2021-10-29 11:59:30 -0300

    feat: handle 404 error

---
+++ src/pages/404.jsx
@@ -0,0 +1,10 @@
+import styles from '../styles/custom404.module.css';
+
+const Custom404 = () => (
+  <section className={`${styles.container} container`}>
+    <h1>404 😕</h1>
+    <small>This page could not be found.</small>
+  </section>
+);
+
+export default Custom404;

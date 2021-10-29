4a4b20b76ad170c88d1a571f565d6eb3d3f9883a
DanielGustavo <dg520589@gmail.com>
2021-10-29 11:34:42 -0300

    feat: change the favicon.ico, and add a title and a description

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,3 +1,5 @@
+import Head from 'next/head';
+
 import Header from '../components/Header';
 
 import '../styles/globals.css';
@@ -5,6 +7,14 @@ import '../styles/globals.css';
 function MyApp({ Component, pageProps }) {
   return (
     <>
+      <Head>
+        <title>Github Searcher</title>
+        <meta
+          name="description"
+          content="Github Searcher is a project made with NextJs and uses the Github API to fetch data from a Github user and display it in a handsome and responsive design."
+        />
+      </Head>
+
       <Header />
       <Component {...pageProps} />
     </>
